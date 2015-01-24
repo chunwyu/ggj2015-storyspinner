dbe0f50ad5217b90f16e83f1abf80db0f388c0fe
Chun Yu <fixception@gmail.com>
2015-01-24 15:16:53 -0800

    Debug tools, Move to Scripts, Dealing Goals
    - Added debug assert tools
    - Added debug card rendering
    - Got Card Prefab with text element
    - Moved the rest of the scripts to Scripts folder
    - Dealing goals

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -17,15 +17,21 @@ public enum GameState
 public class Game : MonoBehaviour {
     private int HandSizeLimit = 7;
 
-    public List<Player> players;
+    private List<Player> players;
+    private Queue<Player> turnQueue;
+    public Player currentPlayer;
+
     public GameObject playerObj;
     public GameState gameState;
     public List<CardData> deck;
+    public List<CardData> goalDeck;
 
 	// Use this for initialization
 	void Start () {
         deck = new List<CardData>();
+        goalDeck = new List<CardData>();
         players = new List<Player>();
+        turnQueue = new Queue<Player>();
 
         // Placeholder: starting at DrawCards until net connection code is in
         AddPlayer("testPlayer", true);
@@ -43,7 +49,11 @@ public class Game : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (gameState == GameState.Loading)
+        if (gameState == GameState.MainMenu)
+        {
+
+        }
+        else if (gameState == GameState.Loading)
         {
             LoadGame();
             gameState = GameState.DrawCards;
@@ -53,22 +63,55 @@ public class Game : MonoBehaviour {
             DrawCards();
             gameState = GameState.DrawGoals;
         }
+        else if (gameState == GameState.DrawGoals)
+        {
+            DrawGoals();
+
+            foreach (Player p in players)
+            {
+                turnQueue.Enqueue(p);
+            }
+
+            gameState = GameState.MakeStory;
+        }
+        else if (gameState == GameState.MakeStory)
+        {
+            ProcessTurn();
+        }
 	}
+
+    void ProcessTurn()
+    {
+        if (currentPlayer == null)
+        {
+            currentPlayer = turnQueue.Dequeue();
+            turnQueue.Enqueue(currentPlayer);
+        }
+    }
 
     void LoadGame()
     {
+        // TODO: Load cards from database instead of this placeholder
         for (int i = 0; i < 40; i++)
         {
             CardData c = new CardData();
-            c.type = WordType.Noun;
+            c.type = CardType.Noun;
             c.title = string.Format("Test card {0}", i);
             deck.Add(c);
+        }
+
+        for (int i = 0; i < 40; i++)
+        {
+            CardData c = new CardData();
+            c.type = CardType.Goal;
+            c.title = string.Format("Test goal {0}", i);
+            goalDeck.Add(c);
         }
     }
 
     void DealCard(Player p)
     {
-        // RPC call here
+        // This is host-side code; p.AddCard should be an RPC call here
 
         if (deck.Count > 0)
         {
@@ -78,14 +121,33 @@ public class Game : MonoBehaviour {
         }
     }
 
+    void DealGoal(Player p)
+    {
+        if (goalDeck.Count > 0)
+        {
+            CardData nextGoal = goalDeck[goalDeck.Count - 1];
+            goalDeck.RemoveAt(goalDeck.Count - 1);
+            p.AddGoal(nextGoal);
+        }
+        // TODO else { reshuffle }
+    }
+
     void DrawCards()
     {
         foreach (Player p in players)
         {
-            for (int i = p.hand.Count; i <= HandSizeLimit; i++)
+            for (int i = p.hand.Count; i < HandSizeLimit; i++)
             {
                 DealCard(p);
             }
+        }
+    }
+
+    void DrawGoals()
+    {
+        foreach (Player p in players)
+        {
+            DealGoal(p);
         }
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour {
+
+    public List<CardData> hand;
+    public List<CardData> goals;
+    public bool isYou;
+    public string playerName;
+    public Game game;
+    int points;
+
+	// Use this for initialization
+	void Start () {
+        hand = new List<CardData>();
+        goals = new List<CardData>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    for (int i = 0; i < hand.Count; i++)
+        {
+        }
+	}
+
+    void OnGUI()
+    {
+        if (isYou)
+        {
+            if (hand.Count == 0)
+            {
+                GUI.Label(new Rect(10, 10, 200, 20), "HAND IS EMPTY");
+            }
+
+            for (int i = 0; i < hand.Count; i++)
+            {
+                GUI.Label(new Rect(10, 10 + 25*i, 200, 20), hand[i].ToString());
+            }
+
+            GUI.skin.button.wordWrap = true;
+            for (int i = 0; i < hand.Count; i++)
+            {
+                GUI.Button(new Rect(20 + 85 * i, 375, 75, 140), hand[i].ToString());
+            }
+
+            for (int i = 0; i < goals.Count; i++)
+            {
+                GUI.Button(new Rect(720 + 110 * i, 375, 90, 140), goals[i].ToString());
+            }
+        }
+    }
+
+    public void AddGoal(CardData c)
+    {
+        if (isYou)
+        {
+
+        }
+
+        goals.Add(c);
+    }
+
+    public void AddCard(CardData c)
+    {
+        if (isYou)
+        {
+            GameObject cardObject = (GameObject)GameObject.Instantiate(Resources.Load("Card"), Vector3.zero, Quaternion.identity);
+            Card cardScript = cardObject.GetComponent<Card>();
+            cardScript.data = c;
+            cardScript.Init();
+        }
+
+        hand.Add(c);
+    }
+}
