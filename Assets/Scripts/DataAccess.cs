ae31099074507e1ac5ddbff771a4f7205651f879
molenubar <molenubar@hotmail.com>
2015-01-24 16:50:42 -0800

    Finished Json Card transformations

--- Assets/Scripts/DataAccess.cs
+++ Assets/Scripts/DataAccess.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class DataAccess : MonoBehaviour {
 
-    public List<CardData> readCard()
+    public static List<CardData> readCard()
     {
         string data = string.Empty;
         List<CardData> cardList = new List<CardData>();
@@ -45,12 +45,37 @@ public class DataAccess : MonoBehaviour {
     
     public static string GetJSONfromCard (CardData card)
     {
-    
+        return string.Format(" {\"CardArray\": [{\"title\": \"?title{0}\",\"description\": \"?description{1}\",\"type\": type{2},\"graphicPath\": \"graphicPath{3}\"}", card.title, card.description, card.type, card.graphicPath);
     }
     
     public static CardData GetCardFromJSON (string data)
     {
-    
+        JSONObject card = new JSONObject(data);
+        CardData newCard = new CardData();
+
+        foreach (JSONObject cardElements in card.list)
+        {
+                CardType x = CardType.Noun;
+
+                switch ((int)cardElements.GetField("type").n)
+                {
+                    case 0: x = CardType.Noun; break;
+                    case 1: x = CardType.Verb; break;
+                    case 2: x = CardType.Adjective; break;
+                    case 3: x = CardType.Goal; break;
+                }
+
+                newCard = new CardData()
+                {
+                    title = cardElements.GetField("title").str,
+                    description = cardElements.GetField("description").str,
+                    graphicPath = cardElements.GetField("graphicPath").str,
+                    type = x
+
+                };
+        }
+
+        return newCard;
     }
 
             
