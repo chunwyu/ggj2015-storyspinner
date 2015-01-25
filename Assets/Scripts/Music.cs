b15c008398642e7c9d89747d32dd26fef8ce128b
molenubar <molenubar@hotmail.com>
2015-01-25 13:17:48 -0800

    Mute and Change Music buttons implemented

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -1,23 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Music : MonoBehaviour {
 
-    public AudioSource source;
-
+    private AudioSource source;
+    private AudioClip music;
+    private int musicPosition = 0;
+    private List<string> musicList = new List<string>();
 	// Use this for initialization
 	void Start () {
 
         source = (AudioSource)gameObject.AddComponent("AudioSource");
-        AudioClip music;
-        music = (AudioClip)Resources.Load("Music/Kites");
-        source.clip = music;
         source.loop = true;
-        source.Play();
+
+        musicList.Add("Music/Kites");
+        musicList.Add("Music/11_Temple_of_Rain");
+        musicList.Add("Music/layer1");
+        musicList.Add("Music/layer2");
+        musicList.Add("Music/layer3");
+        musicList.Add("Music/layer4");
+
+        music = PickMusic(musicList[musicPosition]);
+        PlayMusic(music);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void PlayMusic(AudioClip musicFile)
+    {
+        source.clip = musicFile;
+        source.Play();
+    }
+
+    AudioClip PickMusic(string resourceName)
+    {
+        return (AudioClip)Resources.Load(resourceName);
+    }
+
+    public void IncrementMusicPosition()
+    {
+        if (musicPosition == (musicList.Count - 1))
+        {
+            musicPosition = 0;
+        }
+        else
+        {
+            musicPosition++;
+        }
+        music = PickMusic(musicList[musicPosition]);
+        PlayMusic(music);
+    }
+
+    public void MuteMusic()
+    {
+        source.Stop();
+    }
+
 }
